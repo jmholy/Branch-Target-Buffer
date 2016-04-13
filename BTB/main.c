a7e0ee1f80f23524a81066af8fdd905680d6c6c6
jmholy <jemarho@msn.com>
2016-04-13 14:54:59 -0700

    Worked on Main Setup

--- BTB/main.c
+++ BTB/main.c
@@ -5,9 +5,34 @@ Jordan Little*/
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-int main()
+int main(void)
 {
+	FILE *input = NULL;
+	int sizeBTB = 0;
+	char *filename;
+	char *curPC[1024], *desPC[1024], *pred[1024], *busy[1024];
+	char *address;
+	int curAddress = 0, prevAddress = 0;
+	int rightPred = 0, wrongPred = 0, hitrate = 0;
+	printf("Please enter the source file: ");
+	scanf("%s", filename);
+	printf("Please enter the size of BTB wanted: ");
+	scanf("%d", &sizeBTB);
+	input = fopen(filename, "r");
+	while (!feof)
+	{
+		fscanf(input, "%s", address);
+		/*Brian's conversion from Hex to Dec*/
+		if (curAddress == (prevAddress + 4)) // Not Branch
+		{ 
+			
+		}
+		else //Branch
+		{
 
-	return 1;
+		}
+	}
+	return 0;
 }
